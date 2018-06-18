dd52e94af4dce52455909ac3a7ea567f73b3249b
Sean Barnard <sbarnard@neworbit.co.uk>
2018-06-18 16:10:14 +0100

    Added security headers to pass on https://securityheaders.com

--- app/app.js
+++ app/app.js
@@ -3,6 +3,13 @@ const port = 3000;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
+  response.setHeader('Strict-Transport-Security', 'max-age=31536000');
+  response.setHeader('Content-Security-Policy', `script-src 'self'`);
+  response.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  response.setHeader('X-Xss-Protection', '1; mode=block');
+  response.setHeader('X-Content-Type-Options', 'nosniff');
+  response.setHeader('Referrer-Policy', 'strict-origin');
+  response.writeHead(200);
   response.end("Hello Node.js Server!");
 };
 
